ff6152d4c8da047475a810cbf359b1e750b96634
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2025-08-28 03:39:29 +0600

    heart functionality added

---
+++ index.js
@@ -0,0 +1,56 @@
+// heart count handler
+function handleHeartClick() {
+    
+    const heartCount = parseInt(document.getElementById('heart-count').innerText);
+    const updatedHeartCount = heartCount + 1;
+    return document.getElementById('heart-count').innerText=updatedHeartCount;
+    
+    
+    
+}
+
+document.getElementById('heart-btn1').addEventListener('click', function(e) {
+    e.preventDefault
+    handleHeartClick();
+    
+});
+document.getElementById('heart-btn2').addEventListener('click', function(e) {
+    e.preventDefault
+    handleHeartClick();
+    
+});
+document.getElementById('heart-btn3').addEventListener('click', function(e) {
+    e.preventDefault
+    handleHeartClick();
+    
+});
+document.getElementById('heart-btn4').addEventListener('click', function(e) {
+    e.preventDefault
+    handleHeartClick();
+    
+});
+document.getElementById('heart-btn5').addEventListener('click', function(e) {
+    e.preventDefault
+    handleHeartClick();
+    
+});
+document.getElementById('heart-btn6').addEventListener('click', function(e) {
+    e.preventDefault
+    handleHeartClick();
+    
+});
+document.getElementById('heart-btn7').addEventListener('click', function(e) {
+    e.preventDefault
+    handleHeartClick();
+    
+});
+document.getElementById('heart-btn8').addEventListener('click', function(e) {
+    e.preventDefault
+    handleHeartClick();
+    
+});
+document.getElementById('heart-btn9').addEventListener('click', function(e) {
+    e.preventDefault
+    handleHeartClick();
+    
+});
