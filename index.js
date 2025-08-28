92b8637cc893273b422449216f197c6655f56e8e
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2025-08-28 07:52:20 +0600

    alert functionality and validation added

--- index.js
+++ index.js
@@ -61,16 +61,16 @@ const callButtons = document.querySelectorAll('.call-btn');
 for (let callBtn of callButtons) {
     callBtn.addEventListener('click', function (e) {
         e.preventDefault;
-        alert('Calling is not supported in this demo. Please copy the number and call manually.');
-        const amount = parseInt(document.getElementById('coins').innerText);
-        const remaining = amount - 20;
-        document.getElementById('coins').innerText = remaining;
-        
         const serviceTitle = document.querySelector('.card-title').innerText;
         const serviceNumber = document.querySelector('.service-number').innerText;
         const date = new Date().toLocaleTimeString();
-
-
+        if (parseInt(document.getElementById('coins').innerText) < 20) {
+            return alert('Insufficient coins to make a call.At least 20 coins are required to make a call.');
+        }
+        alert(`Calling ${serviceTitle}  ${serviceNumber}...`);
+        const amount = parseInt(document.getElementById('coins').innerText);
+        const remaining = amount - 20;
+        document.getElementById('coins').innerText = remaining;
         const callHistories = document.getElementById('call-histories');
         const newHistory = document.createElement('div');
         newHistory.innerHTML = `<div class=" px-4 py-4 rounded-lg mb-2  bg-[#fafafa]">
