207eddf4bdc0099a64e41542206e008e4e18ea67
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2025-08-28 07:10:17 +0600

    call functionality coin deduction and add to call history functionality added

--- index.js
+++ index.js
@@ -1,56 +1,86 @@
 // heart count handler
 function handleHeartClick() {
-    
+
     const heartCount = parseInt(document.getElementById('heart-count').innerText);
     const updatedHeartCount = heartCount + 1;
-    return document.getElementById('heart-count').innerText=updatedHeartCount;
-    
-    
-    
+    return document.getElementById('heart-count').innerText = updatedHeartCount;
+
+
+
 }
 
-document.getElementById('heart-btn1').addEventListener('click', function(e) {
+document.getElementById('heart-btn1').addEventListener('click', function (e) {
     e.preventDefault
     handleHeartClick();
-    
+
 });
-document.getElementById('heart-btn2').addEventListener('click', function(e) {
+document.getElementById('heart-btn2').addEventListener('click', function (e) {
     e.preventDefault
     handleHeartClick();
-    
+
 });
-document.getElementById('heart-btn3').addEventListener('click', function(e) {
+document.getElementById('heart-btn3').addEventListener('click', function (e) {
     e.preventDefault
     handleHeartClick();
-    
+
 });
-document.getElementById('heart-btn4').addEventListener('click', function(e) {
+document.getElementById('heart-btn4').addEventListener('click', function (e) {
     e.preventDefault
     handleHeartClick();
-    
+
 });
-document.getElementById('heart-btn5').addEventListener('click', function(e) {
+document.getElementById('heart-btn5').addEventListener('click', function (e) {
     e.preventDefault
     handleHeartClick();
-    
+
 });
-document.getElementById('heart-btn6').addEventListener('click', function(e) {
+document.getElementById('heart-btn6').addEventListener('click', function (e) {
     e.preventDefault
     handleHeartClick();
-    
+
 });
-document.getElementById('heart-btn7').addEventListener('click', function(e) {
+document.getElementById('heart-btn7').addEventListener('click', function (e) {
     e.preventDefault
     handleHeartClick();
-    
+
 });
-document.getElementById('heart-btn8').addEventListener('click', function(e) {
+document.getElementById('heart-btn8').addEventListener('click', function (e) {
     e.preventDefault
     handleHeartClick();
-    
+
 });
-document.getElementById('heart-btn9').addEventListener('click', function(e) {
+document.getElementById('heart-btn9').addEventListener('click', function (e) {
     e.preventDefault
     handleHeartClick();
-    
+
 });
+
+// call button handler
+
+const callButtons = document.querySelectorAll('.call-btn');
+for (let callBtn of callButtons) {
+    callBtn.addEventListener('click', function (e) {
+        e.preventDefault;
+        alert('Calling is not supported in this demo. Please copy the number and call manually.');
+        const amount = parseInt(document.getElementById('coins').innerText);
+        const remaining = amount - 20;
+        document.getElementById('coins').innerText = remaining;
+        
+        const serviceTitle = document.querySelector('.card-title').innerText;
+        const serviceNumber = document.querySelector('.service-number').innerText;
+        const date = new Date().toLocaleTimeString();
+
+
+        const callHistories = document.getElementById('call-histories');
+        const newHistory = document.createElement('div');
+        newHistory.innerHTML = `<div class=" px-4 py-4 rounded-lg mb-2  bg-[#fafafa]">
+           <h3 class="font-semibold">${serviceTitle}</h3>
+                <div class="flex justify-between  ">
+                    <p>${serviceNumber}</p>
+                    <p>${date}</p>
+                </div>
+        
+        </div>`;
+        callHistories.appendChild(newHistory);
+    });
+}
